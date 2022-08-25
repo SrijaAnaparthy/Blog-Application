52ade7e95c4d484bcd8f3493fc49f76e81a0bb1a
SrijaAnaparthy <86790568+SrijaAnaparthy@users.noreply.github.com>
2022-08-25 18:24:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -185,5 +185,5 @@ app.post('/addad',upload.single('file'),function(req,res)
     })
     }
 })
-app.listen(process.env.PORT || 3200,function(){console.log('server running on 3600')});
+app.listen(process.env.PORT || 3200,function(){console.log('server running on 3200')});
 // app.listen(3600,function(){console.log('server running on 3600')})
